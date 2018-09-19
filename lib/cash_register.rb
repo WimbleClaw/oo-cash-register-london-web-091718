7a6f3a81fe0b0326d1c6963c1b481143c4bd68bd
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-19 17:28:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,13 @@ attr_accessor :total, :discount
   end
 
   def apply_discount
-    @total = @total - @total * @discount / 100.00
-    "After the discount, the total comes to $#{@total.to_i}."
+    if @discount.to_i == 0 
+      "There is no discount to apply."
+    else 
+      @total = @total - @total * @discount / 100.00
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
   end
+
 
 end
